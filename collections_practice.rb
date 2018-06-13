dba10a8e60ce0d18d6821e1ea6c665c580ecaa40
Valerie Moore <periwinkle17@outlook.com>
2018-06-13 14:27:11 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -34,7 +34,7 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.collect do |word| word != array[1] : word + "s" 
-  #new_a = old_a.map { |x| (x=="b" ? x+"!" : x) }
+  array.collect do |word| word !== array[1] : word + "s" 
+  #              { |x| (x=="b" ? x+"!" : x) }
   end
 end
